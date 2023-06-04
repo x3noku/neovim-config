124238fdd1e780786b9007b5c39124542b86f6eb
x3noku <x3noku@gmail.com>
2023-06-05 00:34:26 +0300

    Add folding, limeblame and flutter tools

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,14 @@
 local opt = vim.opt
+local o = vim.o
 
 opt.shiftwidth = 4 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true })
 opt.spelllang = { "en" }
 opt.tabstop = 4 -- Number of spaces tabs count for
 opt.wrap = true -- Disable line wrap
+
+o.foldcolumn = "1"
+o.foldlevel = 99
+o.foldlevelstart = 99
+o.foldenable = true
+o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldclose:"
---
+++ lua/plugins/flutter-tools.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "akinsho/flutter-tools.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "stevearc/dressing.nvim",
+        },
+    },
+}
