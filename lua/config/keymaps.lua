cedd9e4adccddb04f193458bc17ff23b580d49a6
x3noku <x3noku@gmail.com>
2023-09-26 01:17:15 +0300

    Add split keymapings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,18 @@ map("n", "<C-q>", ":qa<CR>", { desc = "Quit all" })
 
 map("n", "{", ":BufferLineMovePrev<CR>", { desc = "Move buffer left" })
 map("n", "}", ":BufferLineMoveNext<CR>", { desc = "Move buffer right" })
+
+map("n", "<A-h>", require("smart-splits").resize_left)
+map("n", "<A-j>", require("smart-splits").resize_down)
+map("n", "<A-k>", require("smart-splits").resize_up)
+map("n", "<A-l>", require("smart-splits").resize_right)
+-- moving between splits
+map("n", "<C-h>", require("smart-splits").move_cursor_left)
+map("n", "<C-j>", require("smart-splits").move_cursor_down)
+map("n", "<C-k>", require("smart-splits").move_cursor_up)
+map("n", "<C-l>", require("smart-splits").move_cursor_right)
+-- swapping buffers between windows
+map("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+map("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+map("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+map("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
