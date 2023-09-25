2aebae57cb89396fb4320ba28b40d35426ddaae6
x3noku <x3noku@gmail.com>
2023-09-26 01:07:39 +0300

    Remove rainbow brackets plugin

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,14 +5,7 @@ return {
             autotag = {
                 enable = true,
             },
-            rainbow = {
-                enable = true,
-                extended_mode = false, -- Also highlight non-bracket delimiters like html tags
-                max_file_lines = nil, -- Do not enable for files with more than n lines, int
-                colors = { "#42CAFD", "#EF626C", "#48AD7E", "#429E73", "#8A4277" },
-            },
         },
     },
     { "windwp/nvim-ts-autotag" },
-    { "p00f/nvim-ts-rainbow" },
 }
