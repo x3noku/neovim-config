9d45a0657c7dc412533bf9fe771cac2123d1c6df
x3noku <x3noku@gmail.com>
2023-09-26 01:07:18 +0300

    Add import-cost

---
+++ lua/plugins/import-cost.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "barrett-ruth/import-cost.nvim",
+        build = "sh install.sh yarn",
+        -- if on windows
+        -- build = 'pwsh install.ps1 yarn',
+        config = true,
+    },
+}
