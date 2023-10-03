fc1ead87d3c5034da1f4b6900a4d400f5e89e7ba
x3noku <x3noku@gmail.com>
2023-10-03 23:42:57 +0300

    Replace deprecated null-ls with none-ls

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -61,7 +61,7 @@ return {
     },
     { "jose-elias-alvarez/typescript.nvim" },
     {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         opts = function(_, opts)
             table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
         end,
