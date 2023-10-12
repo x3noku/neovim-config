436c85b72860cf564b67594324cc00e6ba824b5b
x3noku <x3noku@gmail.com>
2023-10-12 22:02:46 +0300

    Enable none-ls support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
         { import = "lazyvim.plugins.extras.lang.tailwind" },
+        { import = "lazyvim.plugins.extras.lsp.none-ls" },
         { import = "lazyvim.plugins.extras.linting.eslint" },
         { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
         { import = "plugins" },
