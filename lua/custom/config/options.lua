55f64eadbba29b271169a5f4f96da0b37a12844f
x3noku <x3noku@gmail.com>
2024-01-04 01:29:10 +0300

    Configure options

--- lua/custom/config/options.lua
+++ lua/custom/config/options.lua
@@ -1,37 +1,49 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.hlsearch = false
-vim.wo.number = true
+-- Cursor
+vim.opt.cursorline = true -- highlight current line
+vim.opt.so = 5 -- cursor margin
 vim.o.mouse = 'a'
--- vim.o.clipboard = 'unnamedplus'
-vim.o.breakindent = true
+
+-- Enumeration
+vim.wo.number = true -- enumeration
+vim.wo.relativenumber = true -- relative enumeration
+vim.wo.signcolumn = 'yes' -- extra space for diagnostic symbols
+
+-- Folding
+vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+
+-- Wrapping
+vim.wo.wrap = true -- soft wrap text
+vim.wo.linebreak = true -- break by special symbols
+vim.wo.list = false -- hide trailing blank symbols
+vim.o.breakindent = true -- save indent when wrapping line
+vim.opt.showbreak = '\\ ' -- virual symbols to show before wrapped line
+
+-- History
 vim.o.undofile = true
+vim.o.updatetime = 250
+
+-- Search
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.wo.signcolumn = 'yes'
-vim.o.updatetime = 250
+vim.o.hlsearch = true
+
+-- Completion
 vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
+
+-- UI
 vim.o.termguicolors = true
-
+vim.opt.colorcolumn = '120' -- visual stop column width
 vim.opt.shiftwidth = 4 -- Size of an indent
--- opt.shortmess:append({ W = true, I = true, c = true })
--- opt.spelllang = { "en" }
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.pumblend = 10 -- opacity for popups
 
--- opt.wrap = true -- Disable line wrap
--- local opt = vim.opt
--- local o = vim.o
---
--- opt.shiftwidth = 4 -- Size of an indent
--- opt.shortmess:append({ W = true, I = true, c = true })
--- opt.spelllang = { "en" }
--- opt.tabstop = 4 -- Number of spaces tabs count for
--- opt.wrap = true -- Disable line wrap
---
--- o.foldcolumn = "1"
--- o.foldlevel = 99
--- o.foldlevelstart = 99
--- o.foldenable = true
--- o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldclose:"
+-- Clipboard
+-- vim.o.clipboard = 'unnamedplus'
