30c09c11c44157a479d6e9ae5c9d048c5bde5331
x3noku <x3noku@gmail.com>
2024-01-03 18:16:14 +0300

    Configure treesitter context

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter-context' },
+  },
   config = function()
     require('nvim-treesitter.configs').setup {
       ensure_installed = {
@@ -28,6 +31,14 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+    }
+
+    require('treesitter-context').setup {
+      max_lines = 3, -- Values <= 0 mean no limit.
+      line_numbers = true,
+      trim_scope = 'outer',
+      mode = 'cursor', -- Choices: 'cursor', 'topline'
+      zindex = 20,
     }
   end,
   build = ':TSUpdate',
