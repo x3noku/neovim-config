cd8b8ade32de7642a9e91efb0c28fe92a4629f68
x3noku <x3noku@gmail.com>
2024-01-03 16:49:09 +0300

    Add colorizer

---
+++ lua/custom/plugins/colorizer.lua
@@ -0,0 +1,22 @@
+return {
+  'NvChad/nvim-colorizer.lua',
+  config = function()
+    require('colorizer').setup {
+      filetypes = {
+        '*',
+        scss = { mode = 'virtualtext' },
+        typescriptreact = { mode = 'background' },
+      },
+      user_default_options = {
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        mode = 'foreground', -- Set the display mode.
+        tailwind = true,
+        sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+        virtualtext = '■',
+        -- update color values even if buffer is not focused
+        -- example use: cmp_menu, cmp_docs
+        always_update = false,
+      },
+    }
+  end,
+}
