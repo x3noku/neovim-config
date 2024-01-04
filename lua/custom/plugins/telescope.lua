9e2781099ef3e42fb1c14f52fff82e140051d419
x3noku <x3noku@gmail.com>
2024-01-04 13:39:06 +0300

    Extract file browser

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -22,32 +22,5 @@ return {
         require('telescope').load_extension 'live_grep_args'
       end,
     },
-    {
-      'nvim-telescope/telescope-file-browser.nvim',
-      dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('telescope').load_extension 'file_browser'
-      end,
-    },
   },
-  config = function()
-    require('telescope').setup {
-      extensions = {
-        file_browser = {
-          theme = 'ivy',
-          grouped = true,
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            ['i'] = {
-              -- your custom insert mode mappings
-            },
-            ['n'] = {
-              -- your custom normal mode mappings
-            },
-          },
-        },
-      },
-    }
-  end,
 }
