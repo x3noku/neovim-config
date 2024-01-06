51008d93b967adb2783c68987360da67191943dc
x3noku <x3noku@gmail.com>
2024-01-06 13:22:19 +0300

    Configure autocmds

--- lua/custom/config/autocmds.lua
+++ lua/custom/config/autocmds.lua
@@ -1,16 +1,81 @@
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+local function augroup(name)
+  return vim.api.nvim_create_augroup('kick_' .. name, { clear = true })
+end
+
+-- highlight text on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup 'highlight_yank',
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
-  pattern = '*',
 })
 
-vim.api.nvim_create_autocmd('VimLeave', { command = '!eslint_d stop' })
+-- stop eslint server when vim is closed
+vim.api.nvim_create_autocmd('VimLeave', {
+  group = augroup 'eslint_stop',
+  command = '!eslint_d stop',
+})
 
-vim.defer_fn(function()
-  -- todo: set theme
-  local themes = require 'custom.config.themes'
-  themes.set(themes.read())
-end, 0)
+-- set theme when vim is loaded
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = augroup 'set_theme',
+  callback = function()
+    vim.defer_fn(function()
+      local themes = require 'custom.config.themes'
+      themes.set(themes.read())
+    end, 0)
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup 'last_loc',
+  callback = function(event)
+    local exclude = { 'gitcommit' }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+  group = augroup 'resize_splits',
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd 'tabdo wincmd ='
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close_with_q',
+  pattern = {
+    'PlenaryTestPopup',
+    'help',
+    'lspinfo',
+    'man',
+    'notify',
+    'qf',
+    'query',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+    'neotest-output',
+    'checkhealth',
+    'neotest-summary',
+    'neotest-output-panel',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
