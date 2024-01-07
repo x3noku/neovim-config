97ac67b4e2143b71bcff5df569c490141214322c
x3noku <x3noku@gmail.com>
2024-01-07 13:34:36 +0300

    Configure keymaps

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,14 +1,24 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
-vim.keymap.set('n', '<C-q>', ':qa<CR>', { desc = 'Quit all' })
+vim.keymap.set('i', 'jj', '<esc>', { silent = true })
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
 
 -- Navigation keymaps
-vim.keymap.set('n', 'k', 'v:count == 0 ? "gk" : "k"', { expr = true, silent = true })
-vim.keymap.set('n', 'j', 'v:count == 0 ? "gj" : "j"', { expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'j', 'v:count == 0 ? "gj" : "j"', { expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Down>', 'v:count == 0 ? "gj" : "j"', { expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'k', 'v:count == 0 ? "gk" : "k"', { expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Up>', 'v:count == 0 ? "gk" : "k"', { expr = true, silent = true })
+
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Better move up' })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Better move down' })
 vim.keymap.set('n', 'N', 'Nzz', { desc = 'Search previous' })
 vim.keymap.set('n', 'n', 'nzz', { desc = 'Search next' })
+
+-- Move lines
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move line up' })
+vim.keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move line down' })
+vim.keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move line up' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
@@ -29,13 +39,12 @@ vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>fg', require('telescope').extensions.live_grep_args.live_grep_args, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
 vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[F]ind [R]esume' })
-vim.keymap.set('n', '<leader>fc', ':noh<CR>', { desc = '[F]ind [C]lear' })
+vim.keymap.set('n', '<leader>fc', '<cmd>noh<cr><esc>', { desc = '[F]ind [C]lear' })
+vim.keymap.set('n', '<esc>', '<cmd>noh<cr><esc>', { desc = '[F]ind [C]lear' })
 
--- File browser
-vim.keymap.set('n', '<leader>b', ':Neotree<CR>', { noremap = true })
-
--- Lazygit
-vim.keymap.set('n', '<space>gg', ':LazyGit<CR>', { noremap = true })
+-- Buffers
+vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev buffer' })
+vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
 
 -- Clipboard
 vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
@@ -49,17 +58,43 @@ vim.keymap.set({ 'n', 'x' }, '<leader>p', '"+p')
 vim.keymap.set({ 'n', 'x' }, '<leader>P', '"+P')
 
 -- Splits
-vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
-vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
-vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
-vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+vim.keymap.set('n', '<C-Left>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<C-Down>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<C-Up>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<C-Right>', require('smart-splits').resize_right)
 
 vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
 
--- vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
--- vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
--- vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
--- vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
+-- Add undo break-points
+vim.keymap.set('i', ',', ',<c-g>u')
+vim.keymap.set('i', '.', '.<c-g>u')
+vim.keymap.set('i', ';', ';<c-g>u')
+
+-- Better indenting
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Managers
+vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+vim.keymap.set('n', '<leader>m', '<cmd>Mason<cr>', { desc = 'Lazy' })
+vim.keymap.set('n', '<leader>b', '<cmd>Neotree<cr>', { noremap = true })
+vim.keymap.set('n', '<space>gg', '<cmd>LazyGit<cr>', { noremap = true })
+
+-- -- Floating terminal
+-- local lazyterm = function() Util.terminal(nil, { cwd = Util.root() }) end
+-- map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+-- map("n", "<leader>fT", function() Util.terminal() end, { desc = "Terminal (cwd)" })
+-- map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
+-- map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
+--
+-- -- Terminal Mappings
+-- map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+-- map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+-- map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+-- map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+-- map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+-- map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+-- map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
