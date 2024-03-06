90d32f920681759f0e25b6c3aa3a89a962beed66
x3noku <x3noku@gmail.com>
2024-03-07 01:11:02 +0300

    Configure neotree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,8 +7,16 @@ return {
     'MunifTanjim/nui.nvim',
   },
   opts = {
+    sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
+    popup_border_style = 'rounded',
     window = {
       position = 'right',
+    },
+    default_component_configs = {
+      modified = {
+        symbol = '✘',
+        highlight = 'NeoTreeModified',
+      },
     },
     filesystem = {
       filtered_items = {
@@ -17,9 +25,20 @@ return {
         hide_gitignored = true,
       },
       follow_current_file = {
-        enabled = false,
+        enabled = true,
       },
       use_libuv_file_watcher = true,
     },
   },
+  config = function(_, opts)
+    require('neo-tree').setup(opts)
+    vim.api.nvim_create_autocmd('TermClose', {
+      pattern = '*lazygit',
+      callback = function()
+        if package.loaded['neo-tree.sources.git_status'] then
+          require('neo-tree.sources.git_status').refresh()
+        end
+      end,
+    })
+  end,
 }
