e1977a399fb1bc773ed492efba0e1f3a9612cbf5
x3noku <x3noku@gmail.com>
2024-04-02 19:32:40 +0300

    Add rename files capabilities

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,65 +1,28 @@
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc and 'LSP: ' .. desc })
   end
 
-  vim.keymap.set('n', '<leader>cr', function()
-    return ':IncRename ' .. vim.fn.expand '<cword>'
-  end, { buffer = bufnr, desc = '[C]ode [R]ename', expr = true })
+  nmap('<leader>cr', ':IncRename ', '[C]ode [R]ename')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
   nmap('<leader>cd', vim.diagnostic.open_float, '[C]ode [D]iagnostic')
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  -- nmap('<leader>wl', function()
-  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  -- end, '[W]orkspace [L]ist Folders')
 
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- todo: rework how lsp servers are defined
 local servers = {
+  stylua = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
       diagnostics = { disable = { 'missing-fields' } },
-    },
-  },
-  tsserver = {
-    typescript = {
-      format = {
-        indentSize = vim.o.shiftwidth,
-        convertTabsToSpaces = vim.o.expandtab,
-        tabSize = vim.o.tabstop,
-      },
-    },
-    javascript = {
-      format = {
-        indentSize = vim.o.shiftwidth,
-        convertTabsToSpaces = vim.o.expandtab,
-        tabSize = vim.o.tabstop,
-      },
-    },
-    completions = {
-      completeFunctionCalls = true,
     },
   },
 }
@@ -98,6 +61,16 @@ return {
           auto_attach = true,
         },
       },
+    },
+    {
+      'antosha417/nvim-lsp-file-operations',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-neo-tree/neo-tree.nvim',
+      },
+      config = function()
+        require('lsp-file-operations').setup()
+      end,
     },
   },
   config = function()
