91b1dc7460b84d2dd95ae82446a02048efe14bf4
x3noku <x3noku@gmail.com>
2024-09-08 12:10:30 +0300

    Update lorem plugin configuration

--- lua/custom/plugins/editor/lorem.lua
+++ lua/custom/plugins/editor/lorem.lua
@@ -1,7 +1,7 @@
 return {
   'derektata/lorem.nvim',
   config = function()
-    require('lorem').setup {
+    require('lorem').opts {
       sentenceLength = 'medium',
       comma_chance = 0.2,
       max_commas_per_sentence = 2,
