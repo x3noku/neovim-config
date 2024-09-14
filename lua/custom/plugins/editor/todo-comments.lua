c4dd7cc9e9f51056e98f11625f3ca1af73a6f859
x3noku <x3noku@gmail.com>
2024-09-14 21:43:15 +0300

    Update todo comments config

--- lua/custom/plugins/editor/todo-comments.lua
+++ lua/custom/plugins/editor/todo-comments.lua
@@ -4,15 +4,17 @@ return {
   opts = {
     signs = true,
     keywords = {
-      TODO = { icon = ' ', alt = { 'ToDo', 'todo' } },
-      FIXME = { icon = ' ', alt = { 'FixMe', 'fixme' } },
-      HACK = { icon = ' ', alt = { 'Hack', 'hack' } },
-      WARN = { icon = ' ', alt = { 'WARNING', 'Warning', 'warning', 'Warn', 'warn' } },
+      TODO = { icon = ' ', color = 'info', alt = { 'ToDo', 'todo' } },
+      FIXME = { icon = ' ', color = 'error', alt = { 'FixMe', 'fixme', 'BUG', 'bug', 'FIXIT', 'FixIt', 'fixit', 'ISSUE', 'issue' } },
+      HACK = { icon = ' ', color = 'warning', alt = { 'Hack', 'hack' } },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'Warning', 'warning', 'Warn', 'warn', 'XXX', 'xxx' } },
       PERF = {
-        icon = ' ',
+        -- icon = ' ',
+        icon = '⏲ ',
         alt = { 'PERFORMANCE', 'OPTIMIZE', 'Perfomance', 'Optimize', 'Perf', 'perfomance', 'optimize', 'perf' },
       },
-      NOTE = { icon = ' ', alt = { 'INFO', 'Note', 'Info', 'note', 'info' } },
+      NOTE = { icon = ' ', alt = { 'INFO', 'HINT', 'Note', 'Info', 'Hint', 'note', 'info', 'hint' } },
+      -- TEST = { icon = '', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
     },
     highlight = {
       multiline = false,
@@ -20,10 +22,20 @@ return {
       keyword = 'wide_bg',
       after = 'bg',
       comments_only = true,
-      pattern = [[(KEYWORDS)(\:|\(.*\)\:?)\s*]],
+      pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]],
     },
     search = {
-      pattern = [[\b(KEYWORDS)]], -- ripgrep regex
+      command = 'rg',
+      args = {
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+      },
+      -- regex that will be used to match keywords.
+      -- don't replace the (KEYWORDS) placeholder
+      pattern = [[\b(KEYWORDS)(\(\w*\))*:]],
     },
     gui_style = {
       bg = 'BOLD',
