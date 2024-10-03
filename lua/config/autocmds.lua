ca364a4c918c20ec20c9c9c2b4dcf522bfa63abb
x3noku <x3noku@gmail.com>
2024-10-04 00:19:10 +0300

    Add new autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,17 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('kick_' .. name, { clear = true })
 end
 
--- highlight text on yank
+-- check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = augroup 'checktime',
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd 'checktime'
+    end
+  end,
+})
+
+-- highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup 'highlight_yank',
   callback = function()
@@ -59,12 +69,12 @@ vim.api.nvim_create_autocmd({ 'VimResized' }, {
   end,
 })
 
--- todo: close telescope with <q> too
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd('FileType', {
   group = augroup 'close_with_q',
   pattern = {
     'PlenaryTestPopup',
+    'grug-far',
     'help',
     'lspinfo',
     'man',
@@ -78,9 +88,80 @@ vim.api.nvim_create_autocmd('FileType', {
     'checkhealth',
     'neotest-summary',
     'neotest-output-panel',
+    'dbout',
+    'gitsigns-blame',
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', {
+      buffer = event.buf,
+      silent = true,
+      desc = 'Quit buffer',
+    })
+  end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'man_unlisted',
+  pattern = { 'man' },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'wrap_spell',
+  pattern = { 'text', 'plaintex', 'typst', 'gitcommit', 'markdown' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- fix conceallevel for json files
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = augroup 'json_conceal',
+  pattern = { 'json', 'jsonc', 'json5' },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+-- auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = augroup 'auto_create_dir',
+  callback = function(event)
+    if event.match:match '^%w%w+:[\\/][\\/]' then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
+
+vim.filetype.add {
+  pattern = {
+    ['.*'] = {
+      function(path, buf)
+        return vim.bo[buf]
+            and vim.bo[buf].filetype ~= 'bigfile'
+            and path
+            and vim.fn.getfsize(path) > vim.g.bigfile_size
+            and 'bigfile'
+            or nil
+      end,
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = augroup 'bigfile',
+  pattern = 'bigfile',
+  callback = function(ev)
+    vim.schedule(function()
+      vim.bo[ev.buf].syntax = vim.filetype.match { buf = ev.buf } or ''
+    end)
   end,
 })
