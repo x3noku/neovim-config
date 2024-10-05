6147f76c60cd0ea4cec7c3263e2ece6cc3879aa6
x3noku <x3noku@gmail.com>
2024-10-05 18:03:49 +0300

    Add image preview and keymaps for neo-tree

--- lua/plugins/ui/neo-tree.lua
+++ lua/plugins/ui/neo-tree.lua
@@ -5,17 +5,52 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
+    {
+      '3rd/image.nvim',
+      dependencies = {
+        { 'vhyrro/luarocks.nvim', opts = { rocks = { 'magick' } } },
+      },
+      opts = {},
+    },
   },
   opts = {
     sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
     popup_border_style = 'rounded',
     window = {
       position = 'right',
+      mappings = {
+        ['l'] = 'open',
+        ['h'] = 'close_node',
+        ['<space>'] = 'none',
+        ['Y'] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+
+            vim.fn.setreg('', path, 'c')
+            vim.fn.setreg('+', path, 'c')
+          end,
+          desc = 'Copy Path to Clipboard',
+        },
+        ['O'] = {
+          function(state)
+            require('lazy.util').open(state.tree:get_node().path, { system = true })
+          end,
+          desc = 'Open with System Application',
+        },
+        ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
+      },
     },
     default_component_configs = {
       modified = {
         symbol = '✘',
         highlight = 'NeoTreeModified',
+      },
+      git_status = {
+        symbols = {
+          unstaged = '󰄱',
+          staged = '󰱒',
+        },
       },
     },
     filesystem = {
@@ -24,9 +59,7 @@ return {
         hide_dotfiles = false,
         hide_gitignored = true,
       },
-      follow_current_file = {
-        enabled = true,
-      },
+      follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
     },
   },
