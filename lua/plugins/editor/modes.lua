fc46c6743009a956bb8b94d69048ca95547e4542
x3noku <x3noku@gmail.com>
2024-12-10 16:21:23 +0300

    Remove visual modes

--- lua/plugins/editor/modes.lua
+++ lua/plugins/editor/modes.lua
@@ -1,5 +1,21 @@
-return {
-  'mvllow/modes.nvim',
-  tag = 'v0.2.0',
-  opts = {},
-}
+return {}
+
+-- return {
+--   'mvllow/modes.nvim',
+--   tag = 'v0.2.0',
+--   opts = {},
+-- }
+
+-- return {
+--   'svampkorg/moody.nvim',
+--   event = { 'ModeChanged', 'BufWinEnter', 'WinEnter' },
+--   opts = {
+--     disabled_filetypes = { 'TelescopePrompt', 'neo-tree-popup' },
+--     bold_nr = false,
+--     recording = {
+--       enabled = true,
+--       pre_registry_text = '[',
+--       post_registry_text = ']',
+--     },
+--   },
+-- }
